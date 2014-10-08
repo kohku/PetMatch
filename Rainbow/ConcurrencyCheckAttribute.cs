14bb58b7c41929f0995681d298ad1182bd08c68e
kohku <kohku@yahoo.com>
2014-10-08 17:25:49 -0500

    Thinking into adding a new attribute for concurrency check.

---
+++ Rainbow/ConcurrencyCheckAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow.Web
+{
+    /// <summary>
+    /// Explicity include the BusinessBase.HasConcurrency method.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = true)]
+    public class ConcurrencyCheckAttribute : Attribute
+    {
+    }
+}
